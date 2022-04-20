ad711df8ebee912036f0d833e38b533e8a45a290
Ilechko <91555962+Ilechko@users.noreply.github.com>
2022-04-20 15:18:02 +0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Piroll'`, () => {
+  it(`should have as title 'piroll'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Piroll');
+    expect(app.title).toEqual('piroll');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Piroll app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('piroll app is running!');
   });
 });
