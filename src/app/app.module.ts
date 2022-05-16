84cd629ec284c13d4c8b32fc30630c31bb0e2b32
Ilechko <91555962+Ilechko@users.noreply.github.com>
2022-05-16 10:33:45 +0300

    merge branch main and part2 in part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoBlockComponent } from './components/video-block/video-block.component';
+import { TabsBlockComponent } from './components/tabs-block/tabs-block.component';
+import { SliderBlockComponent } from './components/slider-block/slider-block.component';
+import { PartnersBlockComponent } from './components/partners-block/partners-block.component';
+import { FormGruopBlockComponent } from './components/form-gruop-block/form-gruop-block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoBlockComponent,
+    TabsBlockComponent,
+    SliderBlockComponent,
+    PartnersBlockComponent,
+    FormGruopBlockComponent,
   ],
   imports: [
     BrowserModule,
