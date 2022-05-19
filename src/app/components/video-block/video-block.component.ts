4220ed233d17379146bfb4ab015ee7d89012e18a
Serhiy Kolesnyk <serhiy.kolesnyk@ag-space.com>
2022-05-19 10:14:34 +0300

    main - VIDEO-BLOCK review

--- src/app/components/video-block/video-block.component.ts
+++ src/app/components/video-block/video-block.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoBlockComponent implements OnInit {
 
+  mainTitle = 'Our Work Process';
+
   constructor() { }
 
   ngOnInit(): void {
